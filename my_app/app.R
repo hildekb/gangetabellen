7d35bb973a7d8262893da9f1c4faf8fdf7a4c4f0
Hilde Kjelgaard Brustad <hilde.k.brustad@gmail.com>
2024-05-07 22:42:08 +0200

    testing app

--- my_app/app.R
+++ my_app/app.R
@@ -1,1 +1,33 @@
 # This is the main file
+
+library(shiny)
+library(bslib)
+
+ui <- page_sidebar(
+  title = "Øve på gangetabellen",
+  sidebar = sidebar(
+    radioButtons(
+      inputId = 'n_stk',
+      label = 'Velg antall regnestykker:',
+      choices = c('5','10','15'),
+      selected = '15'
+    )
+  ),
+  "Regn ut og sjekk svarene",
+  
+  mainPanel(
+    textOutput(outputId = 'AntallStykker'),
+  )
+  
+) 
+
+# -----------------
+
+server = function(input,output, session){
+    output$AntallStykker = renderText({paste0('Du valgte ',input$n_stk,' regnestykker')}
+                
+    
+  )
+}
+
+shinyApp(ui = ui, server = server)
